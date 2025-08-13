a8669d5098e438d3a6c0a3a168c2d8c354a9b6f6
Routersys <machikado.mazoku.2@gmail.com>
2025-08-13 19:23:45 +0900

    更新

--- HeatHazeCustomEffect.cs
+++ HeatHazeCustomEffect.cs
@@ -1,37 +1,40 @@
-﻿using System.IO;
+using System.IO;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using Vortice.Direct2D1;
 using YukkuriMovieMaker.Commons;
 using YukkuriMovieMaker.Player.Video;
 
-namespace YMM4HeatHaze.Effect.Video
+namespace YMM4HeatShimmer.Effect.Video
 {
-    internal class HeatHazeCustomEffect : D2D1CustomShaderEffectBase
+    internal class HeatShimmerCustomEffect : D2D1CustomShaderEffectBase
     {
         public float Strength { set => SetValue((int)EffectImpl.Properties.Strength, value); }
-        public float Speed { set => SetValue((int)EffectImpl.Properties.Speed, value); }
         public float Scale { set => SetValue((int)EffectImpl.Properties.Scale, value); }
-        public float Time { set => SetValue((int)EffectImpl.Properties.Time, value); }
         public float Angle { set => SetValue((int)EffectImpl.Properties.Angle, value); }
+        public float FlowSpeed { set => SetValue((int)EffectImpl.Properties.FlowSpeed, value); }
+        public float BoilSpeed { set => SetValue((int)EffectImpl.Properties.BoilSpeed, value); }
         public float ChromaticAberration { set => SetValue((int)EffectImpl.Properties.ChromaticAberration, value); }
+        public int EnableBlur { set => SetValue((int)EffectImpl.Properties.EnableBlur, value); }
+        public float BlurStrength { set => SetValue((int)EffectImpl.Properties.BlurStrength, value); }
         public int PreventEdgeStretch { set => SetValue((int)EffectImpl.Properties.PreventEdgeStretch, value); }
         public int DebugMode { set => SetValue((int)EffectImpl.Properties.DebugMode, value); }
+        public float Time { set => SetValue((int)EffectImpl.Properties.Time, value); }
 
-        public HeatHazeCustomEffect(IGraphicsDevicesAndContext devices)
+        public HeatShimmerCustomEffect(IGraphicsDevicesAndContext devices)
             : base(Create<EffectImpl>(devices))
         {
         }
 
         [CustomEffect(1)]
-        private class EffectImpl() : D2D1CustomShaderEffectImplBase<EffectImpl>(LoadShaderFromEmbeddedResource("HeatHazeShader.cso"))
+        private class EffectImpl() : D2D1CustomShaderEffectImplBase<EffectImpl>(LoadShaderFromEmbeddedResource("HeatShimmerShader.cso"))
         {
             private ConstantBuffer _constants;
 
             private static byte[] LoadShaderFromEmbeddedResource(string resourceName)
             {
                 var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-                var fullResourceName = $"YMM4HeatHaze.Shaders.{resourceName}";
+                var fullResourceName = $"YMM4HeatShimmer.Shaders.{resourceName}";
                 using var stream = assembly.GetManifestResourceStream(fullResourceName) ?? throw new FileNotFoundException($"Shader resource '{fullResourceName}' not found.");
                 using var ms = new MemoryStream();
                 stream.CopyTo(ms);
@@ -44,43 +47,56 @@ namespace YMM4HeatHaze.Effect.Video
             struct ConstantBuffer
             {
                 public float Strength;
-                public float Speed;
                 public float Scale;
-                public float Time;
                 public float Angle;
+                public float FlowSpeed;
+                public float BoilSpeed;
                 public float ChromaticAberration;
+                public int EnableBlur;
+                public float BlurStrength;
                 public int PreventEdgeStretch;
                 public int DebugMode;
+                public float Time;
+                private float _padding;
             }
 
             public enum Properties
             {
                 Strength,
-                Speed,
                 Scale,
-                Time,
                 Angle,
+                FlowSpeed,
+                BoilSpeed,
                 ChromaticAberration,
+                EnableBlur,
+                BlurStrength,
                 PreventEdgeStretch,
-                DebugMode
+                DebugMode,
+                Time
             }
 
             [CustomEffectProperty(PropertyType.Float, (int)Properties.Strength)]
             public float Strength { get => _constants.Strength; set { _constants.Strength = value; UpdateConstants(); } }
-            [CustomEffectProperty(PropertyType.Float, (int)Properties.Speed)]
-            public float Speed { get => _constants.Speed; set { _constants.Speed = value; UpdateConstants(); } }
             [CustomEffectProperty(PropertyType.Float, (int)Properties.Scale)]
             public float Scale { get => _constants.Scale; set { _constants.Scale = value; UpdateConstants(); } }
-            [CustomEffectProperty(PropertyType.Float, (int)Properties.Time)]
-            public float Time { get => _constants.Time; set { _constants.Time = value; UpdateConstants(); } }
             [CustomEffectProperty(PropertyType.Float, (int)Properties.Angle)]
             public float Angle { get => _constants.Angle; set { _constants.Angle = value; UpdateConstants(); } }
+            [CustomEffectProperty(PropertyType.Float, (int)Properties.FlowSpeed)]
+            public float FlowSpeed { get => _constants.FlowSpeed; set { _constants.FlowSpeed = value; UpdateConstants(); } }
+            [CustomEffectProperty(PropertyType.Float, (int)Properties.BoilSpeed)]
+            public float BoilSpeed { get => _constants.BoilSpeed; set { _constants.BoilSpeed = value; UpdateConstants(); } }
             [CustomEffectProperty(PropertyType.Float, (int)Properties.ChromaticAberration)]
             public float ChromaticAberration { get => _constants.ChromaticAberration; set { _constants.ChromaticAberration = value; UpdateConstants(); } }
+            [CustomEffectProperty(PropertyType.Int32, (int)Properties.EnableBlur)]
+            public int EnableBlur { get => _constants.EnableBlur; set { _constants.EnableBlur = value; UpdateConstants(); } }
+            [CustomEffectProperty(PropertyType.Float, (int)Properties.BlurStrength)]
+            public float BlurStrength { get => _constants.BlurStrength; set { _constants.BlurStrength = value; UpdateConstants(); } }
             [CustomEffectProperty(PropertyType.Int32, (int)Properties.PreventEdgeStretch)]
             public int PreventEdgeStretch { get => _constants.PreventEdgeStretch; set { _constants.PreventEdgeStretch = value; UpdateConstants(); } }
             [CustomEffectProperty(PropertyType.Int32, (int)Properties.DebugMode)]
             public int DebugMode { get => _constants.DebugMode; set { _constants.DebugMode = value; UpdateConstants(); } }
+            [CustomEffectProperty(PropertyType.Float, (int)Properties.Time)]
+            public float Time { get => _constants.Time; set { _constants.Time = value; UpdateConstants(); } }
         }
     }
 }
